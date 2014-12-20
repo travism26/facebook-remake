f58756030c0cbcaeeba2cdce0dc937bf3968249c
travism26 <travismartin266@gmail.com>
2014-12-20 13:42:23 -0500

    add ember links

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,21 +1,26 @@
 <!doctype html>
 <html lang = "en">
-<head>
-    <meta charset="UTF-8">
-    <title>My Title</title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/main.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Title</title>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/main.css">
+    </head>
 
-<body>
-    @include('layouts.partials.nav')
-    <div class="container">
-        @include('flash::message')
+    <body>
+        @include('layouts.partials.nav')
+        <div class="container">
+            @include('flash::message')
+            {{ URL::asset('assets/css/bootstrap.min.css') }}
 
-        @yield('content')
-    </div>
-<script src="//code.jquery.com/jquery.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-{{--<script>$('#flash-overlay-modal').modal();</script>--}}
-</body>
+            @yield('content')
+        </div>
+        <script src="//code.jquery.com/jquery.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        <!-- ... additional lines truncated for brevity ... -->
+        <script href="{{ URL::asset('js/libs/jquery-1.11.1.min.js') }}"></script>
+        <script href="{{ URL::asset('js/libs/ember.js') }}"></script>
+        <script href="{{ URL::asset('js/libs/handlebars-v1.3.0.js') }}"></script>
+        {{--<script>$('#flash-overlay-modal').modal();</script>--}}
+    </body>
 </html>
