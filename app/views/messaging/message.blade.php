845c50c76e95860de77a3a9c29ec85c850e2b938
travism26 <travismartin266@gmail.com>
2014-12-22 18:23:12 -0500

    create chat service provider and update messaging view

--- app/views/messaging/message.blade.php
+++ app/views/messaging/message.blade.php
@@ -8,47 +8,39 @@
     </head>
     <body>
         <script type="text/x-handlebars">
-            @{{outlet}}
-        </script>
-        <script
-            type="text/x-handlebars"
-            data-template-name="index"
-        >
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <h1>Laravel 4 Chat</h1>
-                        <table class="table table-striped">
-                            @{{#each}}
-                                <tr>
-                                    <td>
-                                        @{{user}}
-                                    </td>
-                                    <td>
-                                        @{{text}}
-                                    </td>
-                                </tr>
-                            @{{/each}}
-                        </table>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <div class="input-group">
-                            <input
-                                type="text"
-                                class="form-control"
-                            />
-                            <span class="input-group-btn">
-                                <button class="btn btn-default">
-                                    Send
-                                </button>
-                            </span>
+                    @{{outlet}}
+                </script>
+                <script type="text/x-handlebars" data-template-name="index">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>Laravel 4 Chat</h1>
+                                <table class="table table-striped">
+                                    @{{#each message in model}}
+                                        <tr>
+                                            <td @{{bind-attr class="message.user_id_class"}}>
+                                                @{{message.user_name}}
+                                            </td>
+                                            <td>
+                                                @{{message.message}}
+                                            </td>
+                                        </tr>
+                                    @{{/each}}
+                                </table>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    @{{input type="text" value=command class="form-control"}}
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default" @{{action "send"}}>Send</button>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </script>
+                </script>
         @include('messaging.partials.jsDependencies')
     </body>
 </html>
