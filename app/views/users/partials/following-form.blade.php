62fd1df82623c27fe2bb7740eb8987987eeaab15
travism26 <travismartin266@gmail.com>
2015-01-10 12:35:38 -0400

    added condition for guest users

--- app/views/users/partials/following-form.blade.php
+++ app/views/users/partials/following-form.blade.php
@@ -1,11 +1,13 @@
-@if($user->isFollowedBy($currentUser))
-    {{ Form::open(['method' =>'DELETE','route' => ['follow_path', $user->id]]) }}
-        {{ form::hidden('userIdToUnfollow', $user->id) }}
-    	<button type="submit" class="btn btn-danger">Unfollow {{ $user->username }}</button>
+@if($signedIn)
+    @if($user->isFollowedBy($currentUser))
+        {{ Form::open(['method' =>'DELETE','route' => ['follow_path', $user->id]]) }}
+            {{ form::hidden('userIdToUnfollow', $user->id) }}
+            <button type="submit" class="btn btn-danger">Unfollow {{ $user->username }}</button>
+        {{ Form::close() }}
+    @else
+    {{ Form::open(['route' => 'follows_path']) }}
+        {{ form::hidden('userIdToFollow', $user->id) }}
+        <button type="submit" class="btn btn-primary">Follow {{ $user->username }}</button>
     {{ Form::close() }}
-@else
-{{ Form::open(['route' => 'follows_path']) }}
-    {{ form::hidden('userIdToFollow', $user->id) }}
-	<button type="submit" class="btn btn-primary">Follow {{ $user->username }}</button>
-{{ Form::close() }}
+    @endif
 @endif
