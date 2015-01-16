2878fcff6b812760ba9bf59c885b9139609d5249
travism26 <travismartin266@gmail.com>
2015-01-15 21:46:25 -0400

    updated status partial

--- app/views/statuses/partials/status.blade.php
+++ app/views/statuses/partials/status.blade.php
@@ -2,6 +2,8 @@
     <div class="pull-left">
         @include('users.partials.avatar', ['user' => $status->user])
     </div>
+
+
     <div class="media-body">
         <h4 class="media-heading">{{ $status->user->username }}</h4>
         <p>{{ $status->present()->timeSincePublished() }}</p>
