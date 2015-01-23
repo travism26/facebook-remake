24673ff0417a991ff85a070091875f903b0d2fb4
travism26 <travismartin266@gmail.com>
2015-01-23 18:54:37 -0400

    added error message for repo not found issue

--- app/views/api/github.blade.php
+++ app/views/api/github.blade.php
@@ -2,11 +2,15 @@
 
 @section('content')
     <h1>Github repo</h1>
-    <div class="row">
-        <ul>
-            @foreach( $repo as $item )
-                <li><a href="https://github.com/{{ $item->{'owner'}->{'login'} }}/{{ $item->{'name'} }}" target="_blank">{{ $item->{'name'} }}</a></li>
-            @endforeach
-        </ul>
-    </div>
+    @if(isset($repo->{'message'}))
+        <h3>Repo not found</h3>
+    @else
+        <div class="row">
+            <ul>
+                @foreach( $repo as $item )
+                    <li><a href="https://github.com/{{ $item->{'owner'}->{'login'} }}/{{ $item->{'name'} }}" target="_blank">{{ $item->{'name'} }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @stop
