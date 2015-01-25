0464da8a3803813700ea113f247d17e0aab1eb70
travism26 <travismartin266@gmail.com>
2015-01-25 04:00:08 -0400

    git repo divs to display data pulled from github

--- app/views/api/github.blade.php
+++ app/views/api/github.blade.php
@@ -13,4 +13,42 @@
             </ul>
         </div>
     @endif
+
+
+    <hr>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-4">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Title</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>description</p>
+                        <ul class="list-inline">
+                            <li>Followers: 4</li>
+                            <li>Favorites: 1337</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Title</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>second app made</p>
+                        <ul class="list-inline">
+                            <li>Followers: 9</li>
+                            <li>Favorites: 0</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 @stop
