6824ce51bd68164eab9fa8c30517747040629322
travism26 <travismartin266@gmail.com>
2015-02-05 01:26:23 -0400

    removed testing information

--- app/views/messaging/google-maps.blade.php
+++ app/views/messaging/google-maps.blade.php
@@ -11,27 +11,17 @@
     <script type="text/javascript">
         //locations
         var newBrunswick = new google.maps.LatLng(46.500, -66.000);
-        var fredericton = new google.maps.LatLng(45.9500, -66.6667);
-        var gesgapegiag = new google.maps.LatLng(48.199, -65.923);
         var marker;
         var map;
 
-        /**
-         * Data for the markers consisting of a name, a LatLng and a zIndex for
-         * the order in which these markers should display on top of each
-         * other.
-         */
-        //var unavailabledates = $.parseJSON('[{"Location":"Fredericton","latitude":"45.9500","longitude":"-66.6667","zIndex":"3"},{"Location":"Gesgapegiag","latitude":"48.199","longitude":"-65.923","zIndex":"4"}]');
         var test = <?php echo json_encode($events, JSON_PRETTY_PRINT) ?>;
-        //document.write(test.length);
-        for(var i =0; i<test.length; i++){
-            //document.write("location: "+ test[i].location + "<br>");
-            document.write("latitude: "+test[i].latitude + "<br>");
-            document.write("longitude: "+test[i].longitude + "<br>");
-            document.write("zIndex: "+test[i].zIndex + "<br>");
-            document.write("title: "+test[i].title + "<br>");
-            //document.write();
-        }
+
+//        for(var i =0; i<test.length; i++){
+//            document.write("latitude: "+test[i].latitude + "<br>");
+//            document.write("longitude: "+test[i].longitude + "<br>");
+//            document.write("zIndex: "+test[i].zIndex + "<br>");
+//            document.write("title: "+test[i].title + "<br>");
+//        }
         var events = [
             ['Fredericton', 45.9500, -66.6667, 3],
             ['Gesgapegiag', 48.199, -65.923, 4],
@@ -52,8 +42,6 @@
         function setMarkers(map, locations){
 
             for( var i =0; i < locations.length; i++){
-                //document.write(locations[i].title);
-                var event = locations[i].title;
                 var latitude = locations[i].latitude;
                 var longitude= locations[i].longitude;
                 var title = locations[i].title;
@@ -82,5 +70,5 @@
         google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
-<div class="row">{{ $location_encoded =  json_encode($events); }}</div>
+{{--<div class="row">{{ $location_encoded =  json_encode($events); }}</div>--}}
 <div id="map-canvas"></div>
