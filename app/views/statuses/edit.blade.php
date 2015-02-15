23d8fcf6e15cf249823a6c45fdf1d0d576ed1b41
travism26 <travismartin266@gmail.com>
2015-02-14 21:58:57 -0500

    added form elements for updating status

--- app/views/statuses/edit.blade.php
+++ app/views/statuses/edit.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.default')
 @section('content')
-<h1>Edit your status</h1>
-{{ $status->id }}
-{{ $status->body }}
-{{ $status->user_id }}
+    <h1>Edit your status</h1>
+    {{ $status->id }}
+    {{ $status->body }}
+    {{ $status->user_id }}
+    <br>
+    <div class="col-md-6">
+    {{ Form::open() }}
+    <!-- Status Form Input -->
+    <div class="form-group">
+        {{ Form::label('status', 'Status:') }}
+        {{ Form::text ('status', $status->body, ['class'=> 'form-control', 'placeholder' => 'Whats on your Mind']) }}
+    </div>
+
+    {{ Form::close() }}
+    </div>
 @stop
