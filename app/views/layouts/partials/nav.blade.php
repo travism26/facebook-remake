354285e331ff67cbf068c02586f5a99ec3b7f259
travism26 <travismartin266@gmail.com>
2015-02-25 15:57:17 -0400

    fixed route error

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -28,9 +28,9 @@
             </a>
             <ul class="dropdown-menu" role="menu">
               <li>{{ link_to_route('profile_path', 'Your Profile', $currentUser->username) }}</li>
-              <li>{{ link_to_route('geolocation','Location Test') }}</li>
+              <li>{{ link_to_route('profile_path','Location Test') }}</li>
               <li class="divider"></li>
-              <li>{{ link_to_route('google_maps', 'Google Map') }}</li>
+              <li>{{ link_to_route('profile_path', 'Google Map') }}</li>
               <li class="divider"></li>
               <li>{{ link_to_route('logout_path', 'Log Out') }}</li>
             </ul>
