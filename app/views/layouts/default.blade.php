42c343bd44e7a6dc02f0e7245c68a935a44113cb
travism26 <travismartin266@gmail.com>
2015-03-02 23:36:29 -0400

    added meta viewport for mobile

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,6 +2,7 @@
 <html lang = "en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title>My Title</title>
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/main.css">
