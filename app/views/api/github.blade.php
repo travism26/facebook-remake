ba11aa6b72029478c599ecda84fcdbb751e66713
travism26 <travismartin266@gmail.com>
2015-03-17 00:47:00 -0300

    updated view to display data better :)

--- app/views/api/github.blade.php
+++ app/views/api/github.blade.php
@@ -5,51 +5,44 @@
     @if(isset($repo->{'message'}))
         <h3>Repo not found</h3>
     @else
-        <div class="row">
-            <ul>
-                @foreach( $repo as $item )
-                    <li>
-                        <a href="https://github.com/{{ $item['owner']['login'] }}/{{ $item['name'] }}"
-                           target="_blank">{{ $item['name'] }}</a></li>
+        {{--<div class="row">--}}
+        {{--<ul>--}}
+        {{--@foreach( $repo as $item )--}}
+        {{--<li>--}}
+        {{--<a href="https://github.com/{{ $item['owner']['login'] }}/{{ $item['name'] }}"--}}
+        {{--target="_blank">{{ $item['name'] }}</a>--}}
+        {{--</li>--}}
+        {{--@endforeach--}}
+        {{--</ul>--}}
+        {{--</div>--}}
+        {{--@endif--}}
+
+        <hr>
+        <br>
+        @foreach(array_chunk($repo, 3) as $items)
+            <div class="row">
+                @foreach($items as $item)
+                    <div class="col-md-4">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    <a href="https://github.com/{{ $item['owner']['login'] }}/{{ $item['name'] }}"
+                                       target="_blank">{{ $item['name'] }}</a>
+                                </h3>
+                            </div>
+                            <div class="panel-body">
+                                <p>{{ $item['description'] }}</p>
+                                <ul class="list-inline">
+                                    <li>Watchers: {{ $item['watchers_count'] }}</li>
+                                    <li>Favorites: 0</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
-            </ul>
-        </div>
+            </div>
+        @endforeach
+
     @endif
-
-    <hr>
-    <br>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Title</h3>
-                    </div>
-                    <div class="panel-body">
-                        <p>description</p>
-                        <ul class="list-inline">
-                            <li>Followers: 4</li>
-                            <li>Favorites: 1337</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Title</h3>
-                    </div>
-                    <div class="panel-body">
-                        <p>second app made</p>
-                        <ul class="list-inline">
-                            <li>Followers: 9</li>
-                            <li>Favorites: 0</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
 
 @stop
