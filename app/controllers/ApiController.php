e2dedf9cd04eb586e91f0dfb15a68e3c191627b6
travism26 <travismartin266@gmail.com>
2015-03-18 22:03:53 -0300

    updated api controller

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -8,136 +8,137 @@ use GrahamCampbell\GitHub\Facades\GitHub;
 
 class ApiController extends \BaseController {
 
-    protected $markers;
+	protected $markers;
 
-    /**
-     * @param markers $markers
-     */
-    public function __construct(markers $markers)
-    {
-        $this->markers = $markers;
-    }
+	/**
+	 * @param markers $markers
+	 */
+	public function __construct(markers $markers)
+	{
+		$this->markers = $markers;
+	}
 
-    /**
-     * Display a listing of the resource.
-     * GET /api
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        return View::make('api.index');
-    }
+	/**
+	 * Display a listing of the resource.
+	 * GET /api
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return View::make('api.index');
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     * GET /api/create
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /api/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     * POST /api
-     *
-     * @return Response
-     */
-    public function store()
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /api
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     * GET /github/{username}
-     *
-     * @param string $username
-     *
-     * @return Response
-     * @internal param int $id
-     */
-    public function show($username = 'travism26')
-    {
-        // get the user and send request to github
-        // url: "https://api.github.com/users/travism26/repos",
+	/**
+	 * Display the specified resource.
+	 * GET /github/{username}
+	 *
+	 * @param string $username
+	 *
+	 * @return Response
+	 * @internal param int $id
+	 */
+	public function show($username = 'travism26')
+	{
+		// get the user and send request to github
+		// url: "https://api.github.com/users/travism26/repos",
 
-	    $client = new \Github\Client();
-	    $repositories = $client->api('user')->repositories($username);
+		$client       = new \Github\Client();
+		$repositories = $client->api('user')->repositories($username);
 
-	    //dd($repositories);
-	    $gitData = [];
-        return View::make('api.github')->with('repo', $repositories);
-    }
+		//dd($repositories);
+		$gitData = [];
 
-    /**
-     * Show the form for editing the specified resource.
-     * GET /api/{id}/edit
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
+		return View::make('api.github')->with('repo', $repositories);
+	}
 
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /api/{id}/edit
+	 *
+	 * @param  int $id
+	 *
+	 * @return Response
+	 */
+	public function edit($id)
+	{
 
-    /**
-     * Update the specified resource in storage.
-     * PUT /api/{id}
-     * @return Response
-     * @internal param int $id
-     */
-    public function google()
-    {
-        /*
-         * var events = [
-            ['Fredericton', 45.9500, -66.6667, 3],
-            ['Gesgapegiag', 48.199, -65.923, 4],
-            ['Saint John', 45.2796, -66.0628, 2],
-            ['Miramichi', 47.0225, -65.5089, 1]
-        ];
-         */
-        //AUTH code not working...might need to open a support ticket.
-        $authCode = "AIzaSyBq0-11FD2K0CJtw4QEw5EKhzKpivs9Lpw";
-        $locations = array(
-            [
-                'Location' => 'Fredericton',
-                'latitude' => '45.9500',
-                'longitude' => '-66.6667',
-                'zIndex' => '3'
-            ],
-            [
-                'Location' => 'Gesgapegiag',
-                'latitude' => '48.199',
-                'longitude' => '-65.923',
-                'zIndex' => '4'
-            ]
-        );
+	}
 
-        $event = $this->markers->getMarkers();
-        //dd($event);
-        //$location_encoded = json_encode( $locations );
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /api/{id}
+	 * @return Response
+	 * @internal param int $id
+	 */
+	public function google()
+	{
+		/*
+		 * var events = [
+			['Fredericton', 45.9500, -66.6667, 3],
+			['Gesgapegiag', 48.199, -65.923, 4],
+			['Saint John', 45.2796, -66.0628, 2],
+			['Miramichi', 47.0225, -65.5089, 1]
+		];
+		 */
+		//AUTH code not working...might need to open a support ticket.
+		$authCode  = "AIzaSyBq0-11FD2K0CJtw4QEw5EKhzKpivs9Lpw";
+		$locations = array(
+			[
+				'Location'  => 'Fredericton',
+				'latitude'  => '45.9500',
+				'longitude' => '-66.6667',
+				'zIndex'    => '3'
+			],
+			[
+				'Location'  => 'Gesgapegiag',
+				'latitude'  => '48.199',
+				'longitude' => '-65.923',
+				'zIndex'    => '4'
+			]
+		);
 
-        //dd($location_encoded);
-        return View::make('messaging.google-maps')->with('events', $event);
-    }
+		$event = $this->markers->getMarkers();
+		//dd($event);
+		//$location_encoded = json_encode( $locations );
 
-    /**
-     * Remove the specified resource from storage.
-     * DELETE /api/{id}
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+		//dd($location_encoded);
+		return View::make('messaging.google-maps')->with('events', $event);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /api/{id}
+	 *
+	 * @param  int $id
+	 *
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 
 }
