9704c1a1e806eb1e3f44a5ea27188548be2bf1b4
travism26 <travismartin266@gmail.com>
2015-06-13 19:53:25 -0300

    update home page with github api route

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,6 +4,8 @@
     <div class="jumbotron">
         <h1>Welcome to larabook</h1>
         <p>I am remaking facebook using the laravel framework, well a light version of facebook.</p>
+        <p>Note: I added a git api wrapper the route is /github/{USERNAME}</p>
+        <p>example: /github/travism26</p>
         @if( ! $currentUser)
         <p>
             {{ link_to_route('register_path', 'Sign Up!', null, ['class'=> 'btn btn-lg btn-primary']) }}
